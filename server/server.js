ecf366524459d0b9a5e8b6363507bb99fffb7586
Pranya31 <pranya670.be22@chitkara.edu.in>
2024-10-22 15:27:47 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -26,5 +26,5 @@ app.get('/' , (req,res) => {
 
 // APP CONFIG START
 app.listen(port, () => {
-    console.log('Server running on port http://localhost:$(port');
+    console.log(`Server running on port http://localhost:${port}`);
 })
